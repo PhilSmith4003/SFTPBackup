8fa1782040dd357c02116ee9c005f87f1cc138e0
SpiderBitch <larjo9009@pm.me>
2022-04-01 09:44:22 -0500

    Initial planning and outlining completed

---
+++ Main.py
@@ -0,0 +1,10 @@
+# establish sftp connection
+# check sftp connection
+# ask remote server for file/dir list
+# compare directories
+# determine if they're different
+# determine which version is newer
+# upload the new one
+# verify the files using MD5
+# delete the old versions if the sums match
+# disconnect ssh connection
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 SFTPBackup
+
+SFTP Backup program used to automatically backup files to an SFTP server
+
+Required packages:
+pysftp
